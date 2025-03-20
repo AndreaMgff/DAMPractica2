e70707957f8ba70f157eff5ce13a940a56f18a5b
AndreaMgff <95233640+AndreaMgff@users.noreply.github.com>
2025-03-20 15:04:56 +0100

    Terminado

--- DAMPractica2/MainPage.xaml.cs
+++ DAMPractica2/MainPage.xaml.cs
@@ -1,288 +1,100 @@
-﻿namespace DAMPractica2
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Compatibility;
+
+namespace DAMPractica2
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        bool checkForXorO = false;
-        int addOneToScore = 0;
+        private TicTacToe game;
 
         public MainPage()
         {
             InitializeComponent();
+            game = new TicTacToe();
+            ActualizarTurno();
         }
-        void buttonsDisabled()
-        {
-            Button1.IsEnabled = false;
-            Button2.IsEnabled = false;
-            Button3.IsEnabled = false;
-            Button4.IsEnabled = false;
-            Button5.IsEnabled = false;
-            Button6.IsEnabled = false;
-            Button7.IsEnabled = false;
-            Button8.IsEnabled = false;
-            Button9.IsEnabled = false;
-        }
-        void checkForWin()
-        {
-            if (Button1.Text == "X" && Button2.Text == "X" && Button3.Text == "X")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button2.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button4.Text == "X" && Button5.Text == "X" && Button6.Text == "X")
-            {
-                Button4.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button6.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button7.Text == "X" && Button8.Text == "X" && Button9.Text == "X")
-            {
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button8.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button1.Text == "X" && Button4.Text == "X" && Button7.Text == "X")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button4.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button2.Text == "X" && Button5.Text == "X" && Button8.Text == "X")
-            {
-                Button2.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button8.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button3.Text == "X" && Button6.Text == "X" && Button9.Text == "X")
-            {
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button6.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button1.Text == "X" && Button5.Text == "X" && Button9.Text == "X")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button3.Text == "X" && Button5.Text == "X" && Button7.Text == "X")
-            {
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player X won this round", "OK");
-                addOneToScore = int.Parse(LabelXScore.Text);
-                LabelXScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
 
-            // O combinations
-
-            if (Button1.Text == "O" && Button2.Text == "O" && Button3.Text == "O")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button2.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button4.Text == "O" && Button5.Text == "O" && Button6.Text == "O")
-            {
-                Button4.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button6.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button7.Text == "O" && Button8.Text == "O" && Button9.Text == "O")
-            {
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button8.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button1.Text == "O" && Button4.Text == "O" && Button7.Text == "O")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button4.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button2.Text == "O" && Button5.Text == "O" && Button8.Text == "O")
-            {
-                Button2.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button8.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button3.Text == "O" && Button6.Text == "O" && Button9.Text == "O")
-            {
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button6.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button1.Text == "O" && Button5.Text == "O" && Button9.Text == "O")
-            {
-                Button1.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button9.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-            if (Button3.Text == "O" && Button5.Text == "O" && Button7.Text == "O")
-            {
-                Button3.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button5.BackgroundColor = Color.FromRgb(0, 0, 0);
-                Button7.BackgroundColor = Color.FromRgb(0, 0, 0);
-                DisplayAlert("We have a winner", "Player O won this round", "OK");
-                addOneToScore = int.Parse(LabelOScore.Text);
-                LabelOScore.Text = (addOneToScore + 1).ToString();
-                buttonsDisabled();
-            }
-        }
-        void ButtonClick(object sender, EventArgs e)
+        void ButtonClick(object sender, EventArgs e) //Metodo para cuando el jugador pulsa un boton
         {
             Button b = sender as Button;
-            if (!checkForXorO)
+            int row = Microsoft.Maui.Controls.Grid.GetRow(b) - 2;
+            int col = Microsoft.Maui.Controls.Grid.GetColumn(b);
+
+            int turno = game.jugada(row, col);
+            if (turno != -1)
             {
-                b.Text = "X";
-                b.ImageSource = "tresenraya_cruz.png";
-                checkForXorO = true;
+                b.IsEnabled = false;
+                b.ImageSource = turno % 2 == 1 ? ImageSource.FromFile("tresenraya_cruz.png") : ImageSource.FromFile("tresenraya_circulo.png"); //Dependiendo el turno, se asigna una imagen y otraç
+                ActualizarTurno();
             }
-            else
+            int ganador = game.Ganador();
+            if (ganador == 1 || ganador == 2) // Verificar si alguien gano
             {
-                b.Text = "O";
-                b.ImageSource = "tresenraya_circulo.png";
-                checkForXorO = false;
+                DisplayAlert("Ganador", $"Jugador {ganador} ha ganado!", "OK");
+                buttonsDisabled();
+                actualizarMarcador(ganador);
+                limpiarTablero(); 
             }
-            checkForWin();
-            b.IsEnabled = false;
-        }
-        void reset()
-        {
-            Button1.IsEnabled = true;
-            Button2.IsEnabled = true;
-            Button3.IsEnabled = true;
-            Button4.IsEnabled = true;
-            Button5.IsEnabled = true;
-            Button6.IsEnabled = true;
-            Button7.IsEnabled = true;
-            Button8.IsEnabled = true;
-            Button9.IsEnabled = true;
-
-            Button1.Text = "";
-            Button2.Text = "";
-            Button3.Text = "";
-            Button4.Text = "";
-            Button5.Text = "";
-            Button6.Text = "";
-            Button7.Text = "";
-            Button8.Text = "";
-            Button9.Text = "";
-
-            Button1.ImageSource = "";
-            Button2.ImageSource = "";
-            Button3.ImageSource = "";
-            Button4.ImageSource = "";
-            Button5.ImageSource = "";
-            Button6.ImageSource = "";
-            Button7.ImageSource = "";
-            Button8.ImageSource = "";
-            Button9.ImageSource = "";
-
-
-            Button1.Background = Brush.BlueViolet;
-            Button2.Background = Brush.BlueViolet;
-            Button3.Background = Brush.BlueViolet;
-            Button4.Background = Brush.BlueViolet;
-            Button5.Background = Brush.BlueViolet;
-            Button6.Background = Brush.BlueViolet;
-            Button7.Background = Brush.BlueViolet;
-            Button8.Background = Brush.BlueViolet;
-            Button9.Background = Brush.BlueViolet;
-        }
-        void ButtonResetClick(object sender, EventArgs e)
-        {
-            reset();
-        }
-        void ButtonNewGameClick(object sender, EventArgs e)
-        {
-            reset();
-            LabelXScore.Text = "0";
-            LabelOScore.Text = "0";
-        }
-
-        async Task exit()
-        {
-            bool answer = await DisplayAlert("Question?", "Would you like to exit?", "Yes", "No");
-            if (answer)
+            else if (game.PartidaFinalizadaSinGanador()) 
             {
-                Application.Current?.CloseWindow(Application.Current.MainPage.Window);
+                DisplayAlert("Empate", "El juego ha terminado en empate!", "OK");
+                limpiarTablero(); 
             }
         }
-        void ButtonExitClick(object sender, EventArgs e)
+        void ActualizarTurno()
         {
-            exit();
+            if (game.TurnoActual() % 2 == 1)
+            {
+                TurnoScore.Text = "O";
+            }
+            else 
+            {
+                TurnoScore.Text = "X";
+            }
         }
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
 
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
+        void actualizarMarcador(int ganador) // Actualiza el marcador de los jugadores
+        {
+            if (ganador == 1)
+            {
+                LabelXScore.Text = (int.Parse(LabelXScore.Text) + 1).ToString();
+            }
+            else if (ganador == 2)
+            {
+                LabelOScore.Text = (int.Parse(LabelOScore.Text) + 1).ToString();
+            }
+        }
+        void buttonsDisabled()// Deshabilitar todos los botones
+        {
+            foreach (var child in grid.Children)
+            {
+                if (child is Button button)
+                    button.IsEnabled = false;
+            }
+        }
+        void limpiarTablero() //Quita los simbolos del tablero sin afectar al recuento de victorias
+        {
+            foreach (var child in grid.Children)
+            {
+                if (child is Button button)
+                {
+                    button.IsEnabled = true;
+                    button.BackgroundColor = Colors.Black;
+                    button.ImageSource = ImageSource.FromFile("tresenraya.png");  // Pongo otra imagen de fondo base para todos los botones
+                }
+            }
+            game.Reiniciar();  // Reiniciar
+        }
+        public bool PartidaFinalizadaSinGanador() //Metodo qeu comprueba si no hay espacio en el tablero(se pulsaron todos los botones) y no hay ganador, e indica que es empate
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-
 }
